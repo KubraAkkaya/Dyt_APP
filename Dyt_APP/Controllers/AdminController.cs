a961583d91de5b0d92148b33d4deae6ea21a9664
KubraAkkaya <akkaya32kubra@gmail.com>
2023-09-06 21:43:24 +0300

    06.09.2023

--- Dyt_APP/Controllers/AdminController.cs
+++ Dyt_APP/Controllers/AdminController.cs
@@ -62,10 +62,233 @@ namespace Dyt_APP.Controllers
             return RedirectToAction("Index");
         }
 
+        //tüm müşteriler
         public ActionResult ListClient()
         {
             var values = contextAdmin.Clients.ToList();
             return View(values);
+        }
+
+        public ActionResult ClientDetail(int id)
+        {
+            var bmiList = contextAdmin.BMIs.Where(b => b.ClientId == id).ToList();
+            var findedClient = contextAdmin.Clients.Find(id);
+            ViewBag.Name = findedClient.Name;
+            ViewBag.Surname = findedClient.Surname;
+            ViewBag.Age = findedClient.Age;
+            return View("ClientDetail",bmiList);
+        }
+   
+        
+        public ActionResult CalculateBMI(int id)
+        {
+            var findedClient = contextAdmin.Clients.Find(id);
+            var bmiList = contextAdmin.BMIs.Where(b => b.ClientId == id).ToList();
+            ViewBag.BMIList = bmiList;
+            var genders = new SelectList(new List<SelectListItem>
+            {
+                new SelectListItem { Value = "0", Text = "Erkek" },
+                new SelectListItem { Value = "1", Text = "Kadın" }
+            }, "Value", "Text", findedClient.Gender.ToString());
+            ViewBag.Gender = genders;
+            return View("CalculateBMI", findedClient);
+        }
+
+        [HttpPost]
+        public ActionResult CalculateBMI(int id, Gender gender, int age, double weight, double height,Client c)
+        {   
+            var findClient = contextAdmin.Clients.Find(c.ID);
+            var bmiList = contextAdmin.BMIs.Where(b => b.ClientId == id).ToList();
+            ViewBag.BMIList = bmiList;
+            var genders = new SelectList(new List<SelectListItem>
+            {
+                new SelectListItem { Value = "0", Text = "Erkek" },
+                new SelectListItem { Value = "1", Text = "Kadın" }
+            }, "Value", "Text", findClient.Gender.ToString());
+            ViewBag.Gender = genders;
+            
+            if (ModelState.IsValid)
+            {
+                double bmi = weight / (height * height);
+                double minBmi = 0;
+                double maxBmi = 0;
+                double MinBMI = 0;
+                double MaxBMI = 0;
+                double IdealWeight = 0;
+
+                if (age >= 19 && age <= 24)
+                {
+                    if (gender == Gender.Female)
+                    {
+                        minBmi = 18;
+                        maxBmi = 23;
+
+                        MinBMI = minBmi * (height * height);
+                        MaxBMI = maxBmi * (height * height);
+                        IdealWeight = 20 * (height * height);
+                    }
+                    else if (gender == Gender.Male)
+                    {
+                        minBmi = 19;
+                        maxBmi = 24;
+                        MinBMI = minBmi * (height * height);
+                        MaxBMI = maxBmi * (height * height);
+                        IdealWeight = 21 * (height * height);
+                    }
+
+                }
+                else if (age >= 25 && age <= 34)
+                {
+                    if (gender == Gender.Female)
+                    {
+                        minBmi = 19;
+                        maxBmi = 24;
+                        MinBMI = minBmi * (height * height);
+                        MaxBMI = maxBmi * (height * height);
+                        IdealWeight = 21 * (height * height);
+                    }
+                    else if (gender == Gender.Male)
+                    {
+                        minBmi = 20;
+                        maxBmi = 25;
+                        MinBMI = minBmi * (height * height);
+                        MaxBMI = maxBmi * (height * height);
+                        IdealWeight = 22 * (height * height);
+                    }
+
+                }
+                else if (age >= 35 && age <= 44)
+                {
+                    if (gender == Gender.Female)
+                    {
+                        minBmi = 20;
+                        maxBmi = 25;
+                        MinBMI = minBmi * (height * height);
+                        MaxBMI = maxBmi * (height * height);
+                        IdealWeight = 22 * (height * height);
+
+                    }
+                    else if (gender == Gender.Male)
+                    {
+                        minBmi = 21;
+                        maxBmi = 26;
+                        MinBMI = minBmi * (height * height);
+                        MaxBMI = maxBmi * (height * height);
+                        IdealWeight = 23 * (height * height);
+
+                    }
+
+                }
+                else if (age >= 45 && age <= 54)
+                {
+                    if (gender == Gender.Female)
+                    {
+                        minBmi = 21;
+                        maxBmi = 26;
+                        MinBMI = minBmi * (height * height);
+                        MaxBMI = maxBmi * (height * height);
+                        IdealWeight = 23 * (height * height);
+
+                    }
+                    else if (gender == Gender.Male)
+                    {
+                        minBmi = 22;
+                        maxBmi = 27;
+                        MinBMI = minBmi * (height * height);
+                        MaxBMI = maxBmi * (height * height);
+                        IdealWeight = 24 * (height * height);
+
+                    }
+
+                }
+                else if (age >= 55 && age <= 64)
+                {
+                    if (gender == Gender.Female)
+                    {
+                        minBmi = 22;
+                        maxBmi = 27;
+                        MinBMI = minBmi * (height * height);
+                        MaxBMI = maxBmi * (height * height);
+                        IdealWeight = 24 * (height * height);
+
+                    }
+                    else if (gender == Gender.Male)
+                    {
+                        minBmi = 23;
+                        maxBmi = 28;
+                        MinBMI = minBmi * (height * height);
+                        MaxBMI = maxBmi * (height * height);
+                        IdealWeight = 25 * (height * height);
+
+                    }
+
+                }
+                else if (age >= 65)
+                {
+                    if (gender == Gender.Female)
+                    {
+                        minBmi = 23;
+                        maxBmi = 28;
+                        MinBMI = minBmi * (height * height);
+                        MaxBMI = maxBmi * (height * height);
+                        IdealWeight = 25 * (height * height);
+
+                    }
+                    else if (gender == Gender.Male)
+                    {
+                        minBmi = 24;
+                        maxBmi = 29;
+                        MinBMI = minBmi * (height * height);
+                        MaxBMI = maxBmi * (height * height);
+                        IdealWeight = 26 * (height * height);
+
+                    }
+                }
+                
+                ViewBag.BMI = bmi;
+                ViewBag.MinBMI = MinBMI;
+                ViewBag.MaxBMI = MaxBMI;
+                ViewBag.IdealWeight = IdealWeight;
+                ViewBag.Weight = weight;
+                ViewBag.FatMass = c.BMI.FatMass;
+                ViewBag.MuscleMass = c.BMI.MuscleMass;
+                ViewBag.BoneMass = c.BMI.BoneMass;
+                ViewBag.LiquidMass = c.BMI.LiquidMass;
+                ViewBag.InnerFat = c.BMI.InnerFat;
+                ViewBag.Kcal = c.BMI.Kcal;
+                ViewBag.MetabolicAge = c.BMI.MetabolicAge;
+                if (findClient != null)
+                {
+                    var newBMI = new BMI
+                    {
+                     
+                        MaxBMI = MaxBMI,
+                        Weight=weight,
+                        MinBMI = MinBMI,
+                        BmiCalculation = bmi,
+                        IdealWeight = IdealWeight,
+                        CalculationDate = DateTime.Now,
+                        ClientId = c.ID,
+                        FatMass = c.BMI.FatMass,
+                        MuscleMass = c.BMI.MuscleMass,
+                        BoneMass = c.BMI.BoneMass,
+                        LiquidMass = c.BMI.LiquidMass,
+                        InnerFat = c.BMI.InnerFat,
+                        Kcal = c.BMI.Kcal,
+                        MetabolicAge = c.BMI.MetabolicAge
+                    };
+
+                    contextAdmin.BMIs.Add(newBMI);
+                    contextAdmin.SaveChanges();
+                }
+   
+                return View("CalculateBMI", findClient);
+                //return View("AddBMI", new { MaxBMI= MaxBMI, MinBMI= MinBMI, IdealWeight= IdealWeight, weight = weight, height = height });
+            }
+            else
+            {
+                return View();
+            }
         }
 
         public ActionResult MainCalculateBMI()
@@ -74,7 +297,7 @@ namespace Dyt_APP.Controllers
         }
 
         [HttpPost]
-        public ActionResult MainCalculateBMI(Gender gender,int age ,double weight, double height)
+        public ActionResult MainCalculateBMI(Gender gender, int age, double weight, double height)
         {
             // BMI hesaplama kodunu burada kullanın
             double bmi = weight / (height * height);
@@ -215,204 +438,7 @@ namespace Dyt_APP.Controllers
                 }
             }
 
-                return View();
-        }
-        [HttpGet]
-        public ActionResult CalculateBMI(int id)
-        {
-            var findedClient = contextAdmin.Clients.Find(id);
-            var bmiList = contextAdmin.BMIs.Where(b => b.ClientId == id).ToList();
-            ViewBag.BMIList = bmiList;
-            var genders = new SelectList(new List<SelectListItem>
-            {
-                new SelectListItem { Value = "0", Text = "Erkek" },
-                new SelectListItem { Value = "1", Text = "Kadın" }
-            }, "Value", "Text", findedClient.Gender.ToString());
-            ViewBag.Gender = genders;
-            return View("CalculateBMI", findedClient);
-        }
-
-        [HttpPost]
-        public ActionResult CalculateBMI(int id, Gender gender, int age, double weight, double height)
-        {   
-            var Client = contextAdmin.Clients.Find(id);
-            var bmiList = contextAdmin.BMIs.Where(b => b.ClientId == id).ToList();
-            ViewBag.BMIList = bmiList;
-            var genders = new SelectList(new List<SelectListItem>
-            {
-                new SelectListItem { Value = "0", Text = "Erkek" },
-                new SelectListItem { Value = "1", Text = "Kadın" }
-            }, "Value", "Text", Client.Gender.ToString());
-            ViewBag.Gender = genders;
-            
-            if (ModelState.IsValid)
-            {
-                double bmi = weight / (height * height);
-                
-                ViewBag.BMI = bmi;
-                double minBmi = 0;
-                double maxBmi = 0;
-                double MinBMI = 0;
-                double MaxBMI = 0;
-                double IdealWeight = 0;
-
-                if (age >= 19 && age <= 24)
-                {
-                    if (gender == Gender.Female)
-                    {
-                        minBmi = 18;
-                        maxBmi = 23;
-
-                        MinBMI = minBmi * (height * height);
-                        MaxBMI = maxBmi * (height * height);
-                        IdealWeight = 20 * (height * height);
-                    }
-                    else if (gender == Gender.Male)
-                    {
-                        minBmi = 19;
-                        maxBmi = 24;
-                        MinBMI = minBmi * (height * height);
-                        MaxBMI = maxBmi * (height * height);
-                        IdealWeight = 21 * (height * height);
-                    }
-
-                }
-                else if (age >= 25 && age <= 34)
-                {
-                    if (gender == Gender.Female)
-                    {
-                        minBmi = 19;
-                        maxBmi = 24;
-                        MinBMI = minBmi * (height * height);
-                        MaxBMI = maxBmi * (height * height);
-                        IdealWeight = 21 * (height * height);
-                    }
-                    else if (gender == Gender.Male)
-                    {
-                        minBmi = 20;
-                        maxBmi = 25;
-                        MinBMI = minBmi * (height * height);
-                        MaxBMI = maxBmi * (height * height);
-                        IdealWeight = 22 * (height * height);
-                    }
-
-                }
-                else if (age >= 35 && age <= 44)
-                {
-                    if (gender == Gender.Female)
-                    {
-                        minBmi = 20;
-                        maxBmi = 25;
-                        MinBMI = minBmi * (height * height);
-                        MaxBMI = maxBmi * (height * height);
-                        IdealWeight = 22 * (height * height);
-
-                    }
-                    else if (gender == Gender.Male)
-                    {
-                        minBmi = 21;
-                        maxBmi = 26;
-                        MinBMI = minBmi * (height * height);
-                        MaxBMI = maxBmi * (height * height);
-                        IdealWeight = 23 * (height * height);
-
-                    }
-
-                }
-                else if (age >= 45 && age <= 54)
-                {
-                    if (gender == Gender.Female)
-                    {
-                        minBmi = 21;
-                        maxBmi = 26;
-                        MinBMI = minBmi * (height * height);
-                        MaxBMI = maxBmi * (height * height);
-                        IdealWeight = 23 * (height * height);
-
-                    }
-                    else if (gender == Gender.Male)
-                    {
-                        minBmi = 22;
-                        maxBmi = 27;
-                        MinBMI = minBmi * (height * height);
-                        MaxBMI = maxBmi * (height * height);
-                        IdealWeight = 24 * (height * height);
-
-                    }
-
-                }
-                else if (age >= 55 && age <= 64)
-                {
-                    if (gender == Gender.Female)
-                    {
-                        minBmi = 22;
-                        maxBmi = 27;
-                        MinBMI = minBmi * (height * height);
-                        MaxBMI = maxBmi * (height * height);
-                        IdealWeight = 24 * (height * height);
-
-                    }
-                    else if (gender == Gender.Male)
-                    {
-                        minBmi = 23;
-                        maxBmi = 28;
-                        MinBMI = minBmi * (height * height);
-                        MaxBMI = maxBmi * (height * height);
-                        IdealWeight = 25 * (height * height);
-
-                    }
-
-                }
-                else if (age >= 65)
-                {
-                    if (gender == Gender.Female)
-                    {
-                        minBmi = 23;
-                        maxBmi = 28;
-                        MinBMI = minBmi * (height * height);
-                        MaxBMI = maxBmi * (height * height);
-                        IdealWeight = 25 * (height * height);
-
-                    }
-                    else if (gender == Gender.Male)
-                    {
-                        minBmi = 24;
-                        maxBmi = 29;
-                        MinBMI = minBmi * (height * height);
-                        MaxBMI = maxBmi * (height * height);
-                        IdealWeight = 26 * (height * height);
-
-                    }
-                }
-
-                ViewBag.MinBMI = MinBMI;
-                ViewBag.MaxBMI = MaxBMI;
-                ViewBag.IdealWeight = IdealWeight;
-                ViewBag.Weight = weight;
-                if (Client != null)
-                {
-                    var newBMI = new BMI
-                    {
-                        MaxBMI = MaxBMI,
-                        Weight=weight,
-                        MinBMI = MinBMI,
-                        BmiCalculation = bmi,
-                        IdealWeight = IdealWeight,
-                        CalculationDate = DateTime.Now,
-                        ClientId = Client.ID
-                    };
-
-                    contextAdmin.BMIs.Add(newBMI);
-                    contextAdmin.SaveChanges();
-                }
-   
-                return View("CalculateBMI", Client);
-                //return View("AddBMI", new { MaxBMI= MaxBMI, MinBMI= MinBMI, IdealWeight= IdealWeight, weight = weight, height = height });
-            }
-            else
-            {
-                return View();
-            }
+            return View();
         }
 
         /*
--- Dyt_APP/Models/Classes/BMI.cs
+++ Dyt_APP/Models/Classes/BMI.cs
@@ -16,6 +16,14 @@ namespace Dyt_APP.Models.Classes
         public double Weight { get; set; }
         public double BmiCalculation { get; set; }
         public double IdealWeight { get; set; }
+        public double FatMass { get; set; } //yağ kütlesi
+        public double MuscleMass { get; set; } //kas kütlesi
+        public double BoneMass { get; set; } //kemik kütlesi
+        public double LiquidMass { get; set; } //sıvı kütlesi
+        public double InnerFat { get; set; } //iç yağ
+        public double Kcal{ get; set; } //kilokalori
+        public double MetabolicAge{ get; set; } //metabolik yaş
+        
         public DateTime CalculationDate { get; set; } 
 
 
